a58c6cac38fc8ef8099006d925cc4adfd1306bf9
Neeraj <neerajlamba.del@gmail.com>
2018-06-05 20:10:36 -0400

    Initial commit

---
+++ principles-of-software-design/week3/InterfaceAbstract/IMarkovModel.java
@@ -0,0 +1,15 @@
+
+/**
+ * 
+ * @author Neeraj 
+ * @version Nil
+ */
+
+public interface IMarkovModel {
+    public void setTraining(String text);
+    
+    public String getRandomText(int numChars);
+    
+    public void setRandom(int seed);
+    
+}
